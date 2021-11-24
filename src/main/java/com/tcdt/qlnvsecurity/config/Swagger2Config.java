0f0c1e2cfb624debf31e6fe3b4cb6cf7b5a98a64
TECA-PC01\Administrator <abc@gmail.com>
2021-11-24 11:18:40 +0700

    sua config

--- src/main/java/com/tcdt/qlnvsecurity/config/Swagger2Config.java
+++ src/main/java/com/tcdt/qlnvsecurity/config/Swagger2Config.java
@@ -25,7 +25,7 @@ public class Swagger2Config {
 	@Bean
 	public Docket api1() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.tcdt.qlnvservice.controller.catalog"))
+				.apis(RequestHandlerSelectors.basePackage("com.tcdt.qlnvsecurity.controller"))
 				.paths(PathSelectors.regex("/.*")).build().securitySchemes(Lists.newArrayList(apiKey()))
 				.securityContexts(Lists.newArrayList(securityContext())).apiInfo(apiEndPointsInfo())
 				.groupName("Authen");
