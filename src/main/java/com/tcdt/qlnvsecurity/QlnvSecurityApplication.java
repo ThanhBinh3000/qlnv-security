13e332dedd7811e47a32545957fcd285377d564c
bahoang84 <bahoang84@gmail.com>
2022-01-12 15:11:24 +0700

    Update

--- src/main/java/com/tcdt/qlnvsecurity/QlnvSecurityApplication.java
+++ src/main/java/com/tcdt/qlnvsecurity/QlnvSecurityApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
-@EntityScan(basePackages = {"com.tcdt.qlnvsecurity.table" })
+@EntityScan(basePackages = { "com.tcdt.qlnvsecurity.table" })
 public class QlnvSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QlnvSecurityApplication.class, args);
